0b647d611d11ca18cdbbb053c4b6fe571a973bf2
Leonardo <leonardo.tissi.si@gmail.com>
2024-05-10 20:41:39 -0300

    Update - UI

--- app/src/main/java/br/com/leonardo/hexagonapp/ui/components/personalProfileList.kt
+++ app/src/main/java/br/com/leonardo/hexagonapp/ui/components/personalProfileList.kt
@@ -16,7 +16,6 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalMapOf
 import br.com.leonardo.hexagonapp.model.PersonalProfile
 
 @OptIn(ExperimentalMaterial3Api::class)
