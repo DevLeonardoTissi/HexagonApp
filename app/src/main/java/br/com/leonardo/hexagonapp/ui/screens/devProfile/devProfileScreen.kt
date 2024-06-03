46748047e0250e862b99f95cd9c74fd3eaee7eb5
Leonardo <leonardo.tissi.si@gmail.com>
2024-06-03 19:16:55 -0300

    update ui

--- app/src/main/java/br/com/leonardo/hexagonapp/ui/screens/devProfile/devProfileScreen.kt
+++ app/src/main/java/br/com/leonardo/hexagonapp/ui/screens/devProfile/devProfileScreen.kt
@@ -1,5 +1,6 @@
 package br.com.leonardo.hexagonapp.ui.screens.devProfile
 
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -15,6 +16,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -26,6 +28,12 @@ import br.com.leonardo.hexagonapp.ui.components.TypewriterText
 
 @Composable
 fun DevProfileScreen(uiState: DevProfileUiState) {
+
+    val color by animateColorAsState(targetValue = if(uiState.loadingInfo){
+        Color.Red
+    } else {
+        Color.Black
+    })
 
     if (uiState.loadingInfo) {
         Box(
@@ -51,6 +59,7 @@ fun DevProfileScreen(uiState: DevProfileUiState) {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
 
+
             MyAsyncImage(
                 model = uiState.userProfile.avatar_url,
                 description = "",
@@ -64,14 +73,12 @@ fun DevProfileScreen(uiState: DevProfileUiState) {
                             brush = Brush.verticalGradient(
                                 listOf(
                                     Color.White,
-                                    Color.Blue
+                                    color
                                 )
                             )
                         ), CircleShape
                     )
             )
         }
-
     }
-
 }
