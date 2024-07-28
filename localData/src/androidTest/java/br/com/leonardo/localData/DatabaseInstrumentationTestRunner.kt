537c413c420516d012cd5c41dd78681612626f95
Leonardo <leonardo.tissi.si@gmail.com>
2024-07-27 21:14:26 -0300

    refactor instrumented tests for room database with koin

--- localData/src/androidTest/java/br/com/leonardo/localData/DatabaseInstrumentationTestRunner.kt
+++ localData/src/androidTest/java/br/com/leonardo/localData/DatabaseInstrumentationTestRunner.kt
@@ -3,7 +3,7 @@ package br.com.leonardo.localData
 import android.app.Application
 import android.content.Context
 import androidx.test.runner.AndroidJUnitRunner
-
+@Suppress("unused")
 class DatabaseInstrumentationTestRunner : AndroidJUnitRunner() {
     override fun newApplication(
         cl: ClassLoader?,
