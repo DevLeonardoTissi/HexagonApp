29711aced75c1853a46821cebc0dd6308a4e36ed
Leonardo <leonardo.tissi.si@gmail.com>
2024-08-12 21:10:00 -0300

    dependency inversion in repository classes

--- webClient/src/test/java/br/com/leonardo/webClient/WebClientRequisitionsTest.kt
+++ webClient/src/test/java/br/com/leonardo/webClient/WebClientRequisitionsTest.kt
@@ -1,44 +1,16 @@
 package br.com.leonardo.webClient
 
 import br.com.leonardo.webClient.repository.GithubUserRepository
-import br.com.leonardo.webClient.repository.impl.GithubUserRepositoryImpl
-import br.com.leonardo.webClient.services.GithubApiService
 import kotlinx.coroutines.test.runTest
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import org.junit.Rule
 import org.junit.Test
-import org.koin.dsl.module
 import org.koin.test.KoinTest
 import org.koin.test.KoinTestRule
 import org.koin.test.inject
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 
 class WebClientRequisitionsTest : KoinTest {
-
-    private val GITHUB_API_BASE_URL = "https://api.github.com/users/"
-    private val DEVELOPER_NAME = "Leonardo Tissi"
-
-    private val webClientRepositoryModule = module {
-        val client = OkHttpClient.Builder()
-            .addInterceptor(HttpLoggingInterceptor().apply {
-                level = HttpLoggingInterceptor.Level.BODY
-            })
-            .build()
-
-        single {
-            Retrofit.Builder()
-                .baseUrl(GITHUB_API_BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .client(client)
-                .build()
-        }
-        single { get<Retrofit>().create(GithubApiService::class.java) }
-        single<GithubUserRepository>{ GithubUserRepositoryImpl(get()) }
-    }
 
     @get:Rule
     val koinTestRule = KoinTestRule.create {
@@ -46,22 +18,22 @@ class WebClientRequisitionsTest : KoinTest {
         modules(webClientRepositoryModule)
     }
 
+    private val repository by inject<GithubUserRepository>()
+
     @Test
     fun `must return true because the profile exists on github`() = runTest {
-        val repository by inject<GithubUserRepository>()
         assertNotNull(repository.getUserProfileInfo())
     }
 
     @Test
     fun `must return true because the project developer has a specific name`() = runTest {
-        val repository by inject<GithubUserRepository>()
+        val developerName = "Leonardo Tissi"
         val profileInfo = repository.getUserProfileInfo()
-        assertEquals(DEVELOPER_NAME, profileInfo?.name)
+        assertEquals(developerName, profileInfo?.name)
     }
 
     @Test
     fun `must return true because the profile has repositories`() = runTest {
-        val repository by inject<GithubUserRepository>()
         val repositories = repository.getUserRepositoriesInfo()
         assertNotNull(repositories)
     }
